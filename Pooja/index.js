639055277b0e6f2d8c3f24289d199ac75552733a
Vivek-vikku <68743566+Vivek-vikku@users.noreply.github.com>
2020-07-24 20:10:07 +0530

    Update index.js

--- Pooja/index.js
+++ Pooja/index.js
@@ -1,1 +1,8 @@
-alert("Andar jane se pehle Viv ko yaad kijiye... Just kidding... USM");
+var pass = prompt("Please enter the password");
+if(pass == "vivlovespooja") {
+  alert('Andar jane se pehle Viv ko yaad kijiye, just kidding... USM');
+ alert('Welcome baby, i miss uh so much'); 
+}else{
+  alert('Wrong password!');
+  window.close();
+}
