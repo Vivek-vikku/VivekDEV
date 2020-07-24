a9680886dd4c3b3b4b012578c0562a9ed657140e
Vivek-vikku <68743566+Vivek-vikku@users.noreply.github.com>
2020-07-24 20:32:27 +0530

    Delete index.js

--- Pooja/index.js
+++
@@ -1,8 +0,0 @@
-var pass = prompt("Please enter the password");
-if(pass == "vivlovespooja") {
-  alert('Andar jane se pehle Viv ko yaad kijiye, just kidding... USM');
- alert('Welcome baby, i miss uh so much'); 
-}else{
-  alert('Wrong password!');
-  document.getElementsByTagName("BODY")[0].style.display = "none";
-}
